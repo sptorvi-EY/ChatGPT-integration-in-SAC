457559b21a5b218ccdfb712b6f6f36226f2c6ac3
sptorvi-EY <98751009+sptorvi-EY@users.noreply.github.com>
2023-09-28 14:35:05 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
-var ajaxCall = (key, prompt) => {
+var ajaxCall = (key, url, prompt) => {
   return new Promise((resolve, reject) => {
     $.ajax({
       url: url,
       type: "POST",
       dataType: "json",
       data: JSON.stringify({
-        model: "ai-corner-davinci-003",
+        engine: "ai-corner-davinci-003",
         prompt: prompt,
+        temperature: 1,
         max_tokens: 1024,
-        n: 1,
-        temperature: 0.5,
+        top_p: 0.5,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+        best_of: 1,
+        stop: None
       }),
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${key}`,
+        //Authorization: `Bearer ${key}`,
+        "api-key": `${key}`
       },
       crossDomain: true,
       success: function (response, status, xhr) {
